dc47a52c5bb31d9f128f1d23d89f5e82d32e1e4b
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-21 14:24:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,24 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :genre
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
-
+    @songs = []
   end
 
   def all
     @@all
+  end
+
+  def new_song(name, genre)
+    @song = name
+    @genre = genre
+    song.genre = @genre
+    @songs << song
   end
 
 end
